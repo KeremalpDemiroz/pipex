b019eb20aab5882aa2f0e98cfe0b5aa7743562af
keremalpdemiroz <keremalpde@gmail.com>
2025-11-07 16:59:55 +0300

    bug fixed. test1 is latest version. 07-11-25

---
+++ test1/pipex.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kedemiro <kedemiro@student.42istanbul.com. +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 21:04:55 by kedemiro          #+#    #+#             */
+/*   Updated: 2025/11/06 20:11:10 by kedemiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+static void	n_free(char **ptr, int nptr)
+{
+	if (!ptr)
+		return ;
+	while (nptr >= 0)
+	{
+		free(ptr[nptr]);
+		nptr--;
+	}
+	free(ptr);
+}
+
+int	main(int ac, char **av, char **envp)
+{
+	t_list	data;
+	int		i;
+
+	i = 0;
+	if (create_data(&data, ac, av, envp) < 0)
+	{
+		if (ac >= 5)
+			n_free(data.commands, ac -4);
+		return (0);
+	}
+	else
+		mother_process(&data, i);
+	n_free(data.commands, ac -4);
+}
---
+++ test1/utils.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kedemiro <kedemiro@student.42istanbul.com. +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 21:06:44 by kedemiro          #+#    #+#             */
+/*   Updated: 2025/11/06 19:37:24 by kedemiro         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex.h"
+
+void	all_free(char **split)
+{
+	int	a;
+
+	a = 0;
+	if (!split)
+		return ;
+	while (split[a])
+		free (split[a++]);
+	free (split);
+}
+
+char	**path_and_cmd(t_list *data, int i, char *path)
+{
+	char	**combined_path;
+	char	**splited_cmd;
+	char	*joined;
+	int		j;
+
+	j = 0;
+	splited_cmd = ft_split(data->av[i], ' ');
+	combined_path = ft_split(path, ':');
+	if (!combined_path || !splited_cmd[0])
+	{
+		all_free(splited_cmd);
+		all_free(combined_path);
+		return (NULL);
+	}
+	while (combined_path[j])
+	{
+		joined = ft_strjoin(combined_path[j], "/");
+		free(combined_path[j]);
+		combined_path[j] = ft_strjoin(joined, splited_cmd[0]);
+		free(joined);
+		j++;
+	}
+	all_free(splited_cmd);
+	return (combined_path);
+}
+
+char	*choose_envp(t_list *data, const char *wanted)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	if (!data->envp)
+		return (NULL);
+	while (data->envp[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			k = 0;
+			while (data->envp[i][j + k] && wanted[k]
+					&& data->envp[i][j + k] == wanted[k])
+				k++;
+			if (!wanted[k])
+				return (&(data->envp[i][5]));
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
+
+void	pid_error(int pipe_fd[])
+{
+	perror("fork error");
+	close(pipe_fd[0]);
+	close(pipe_fd[1]);
+}
+
+int	create_data(t_list *data, int ac, char **av, char **envp)
+{
+	data->ac = ac;
+	data->av = av;
+	data->envp = envp;
+	data->file_err = 0;
+	data->cmd_err = 0;
+	data->stdout_backup = dup(1);
+	data->stdin_backup = dup(0);
+	data->cmd_split = NULL;
+	if (is_args_ok(data) == 0)
+	{
+		data->infile_fd = open(data->av[1], O_RDONLY);
+		return (0);
+	}
+	else
+		return (-1);
+}
